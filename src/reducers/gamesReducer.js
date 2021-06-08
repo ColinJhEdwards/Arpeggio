41f09653998b55327b0a578a89f580dfce1869b3
Colin Edwards <ColinJhEdwards@gmail.com>
2021-06-07 20:17:34 -0700

    added comments to necessary files to better explain redux and its set up process

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//set up the initial state of the reducer
 const initState = {
   popular: [],
   newGames: [],
@@ -7,8 +8,10 @@ const initState = {
   searched: [],
 };
 
+//create a switch statement for the reducer based of the action.type
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
+    //When an action is used that has a type of FETCH_GAMES the state "popular will recieve the payload from the action of gamesAction.js that has the type of fetch_games "
     case "FETCH_GAMES":
       return { ...state, popular: action.payload.popular };
     default:
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
+// improt combineReducers for adding mutible reducers to the store
 import { combineReducers } from "redux";
+//import the reducer you wish to add
 import gamesReducer from "./gamesReducer";
 
+//give the reducer a name so it can be referenced
 const rootReducer = combineReducers({
   games: gamesReducer,
 });
