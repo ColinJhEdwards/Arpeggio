30c95cf31c2757b25f5163219673d34a0580a7ac
Colin Edwards <ColinJhEdwards@gmail.com>
2021-06-07 17:03:05 -0700

    set up api call for popular games, and first portion of redux

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+  searched: [],
+};
+
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
